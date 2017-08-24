65ac050138471e0c5ee44c9dae1d7ee4bff880fb
Marsel <marsel.perdoci@hotmail.com>
2017-08-24 21:37:38 +0200

    first commit

---
+++ src/main/java/uni/kassel/marsel/fratcher/Main.java
@@ -0,0 +1,16 @@
+package uni.kassel.marsel.fratcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class Main {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("Hello, world");
+    }
+}
