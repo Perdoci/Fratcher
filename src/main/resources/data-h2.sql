db4e7ac534aaa16b3f5535b4f673930d9c8df220
Marsel <marsel.perdoci@hotmail.com>
2017-08-25 23:52:41 +0200

    Testing Heroku without h2webconsole class

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,5 @@
 DELETE FROM USER_;
 
-INSERT INTO USER_STATUS (ID, STATUS, OWNER_ID) VALUES
-  (1, 'stutus-uno', NULL ),
-  (2, 'status-quo',  NULL );
+--INSERT INTO USER_STATUS (ID, STATUS, OWNER_ID) VALUES
+ -- (1, 'stutus-uno', 1 ),
+ -- (2, 'status-quo',  2 );
